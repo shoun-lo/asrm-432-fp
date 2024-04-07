a698f9c34a95acd4dc5036b127cdf91802da2898
shoun-lo <shounlo2@illinois.edu>
2024-04-07 18:13:00 -0500

    adding code to handling data importing

--- logistic.R
+++ logistic.R
@@ -1,12 +1,3 @@
----
-title: "ASRM 432 Final Project"
-names: "Timmy Chinzorig, Shoun Lo, Molly Yetter, Eric Diaz"
-output: pdf_document
-date: "`r Sys.Date()`"
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
 library(tidyverse)
 library(RCurl)
 
@@ -19,5 +10,3 @@ predict_data = read_delim(url2, delim = "\t", escape_double = FALSE, col_names =
 
 url3 = getURL("https://raw.githubusercontent.com/shoun-lo/asrm-432-fp/main/targets.txt")
 target_data = read_delim(url3, delim = "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
-
-```
